fca70b7f4d4e01bff4f31e77d79c64b4010fd446
SuperMiact <506479916@qq.com>
2022-05-25 23:00:35 +0800

    频率分析法

--- encrypt-decrypt/src/main/java/cn/miact/kaiser/KaiserDemo.java
+++ encrypt-decrypt/src/main/java/cn/miact/kaiser/KaiserDemo.java
@@ -5,7 +5,6 @@ package cn.miact.kaiser;
  * @Classname :KaiserDemo
  * @createDate : 2022-05-24 22:14:47
  * @Description : 凯撒加密
- * @use :
  */
 public class KaiserDemo {
     public static void main(String[] args) {
@@ -13,16 +12,46 @@ public class KaiserDemo {
         String input = "Hello World";
         // 把原文右边移动3位
         int key = 3;
+        String s = encrypt(input,key);
+        System.out.println("加密===" + s);
+        String s1 = decrypt(s,key);
+        System.out.println("解密===" + s1);
+    }
+
+    /**
+     * 解密
+     * @param s 密文
+     * @param key 密钥
+     * @return 明文
+     */
+    public static String decrypt(String s, int key) {
+        char[] chars = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char aChar : chars) {
+            int b = aChar;
+            b -= key;
+            char newb = (char) b;
+            sb.append(newb);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 加密
+     * @param input 原文
+     * @return 密文
+     */
+    public static String encrypt(String input,int key) {
         // 把字符串变为字节数组
         char[] chars = input.toCharArray();
         StringBuilder sb = new StringBuilder();
         for (char aChar : chars) {
             int b = aChar;
             // 往右边移动3位
-            b += 3;
+            b += key;
             char newb = (char) b;
             sb.append(newb);
         }
-        System.out.println("密文==="+sb);
+        return sb.toString();
     }
 }
