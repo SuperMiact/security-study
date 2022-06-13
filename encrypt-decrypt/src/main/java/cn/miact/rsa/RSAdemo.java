280de3cce688a6c28839c04fe24a5e80c090df1c
SuperMiact <506479916@qq.com>
2022-06-13 10:07:09 +0800

    私钥加密

--- encrypt-decrypt/src/main/java/cn/miact/rsa/RSAdemo.java
+++ encrypt-decrypt/src/main/java/cn/miact/rsa/RSAdemo.java
@@ -1,6 +1,7 @@
 package cn.miact.rsa;
 
 
+import javax.crypto.Cipher;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
@@ -15,6 +16,8 @@ import java.util.Base64;
  */
 public class RSAdemo {
     public static void main(String[] args) throws Exception {
+        // 原文
+        String input = "啊这";
         // 创建密钥对
         String algorithm = "RSA";
         KeyPairGenerator rsa = KeyPairGenerator.getInstance(algorithm);
@@ -33,9 +36,18 @@ public class RSAdemo {
         String privateKeyEncodedString = Base64.getEncoder().encodeToString(privateKeyEncoded);
         String publicKeyEncodedString = Base64.getEncoder().encodeToString((publicKeyEncoded));
         // 打印公钥和私钥
-        System.out.println(privateKeyEncodedString);
-        System.out.println(publicKeyEncodedString);
+//        System.out.println(privateKeyEncodedString);
+//        System.out.println(publicKeyEncodedString);
 
-
+        // 创建加密对象
+        Cipher cipher = Cipher.getInstance(algorithm);
+        // 对加密进行初始化;
+        // 第一个参数：加密的模式
+        // 第二个参数：你想使用公钥加密还是私钥加密
+        // 我想使用私钥进行加密
+        cipher.init(Cipher.ENCRYPT_MODE,privateKey);
+        // 使用私钥进行加密
+        byte[] bytes = cipher.doFinal(input.getBytes());
+        System.out.println(Base64.getEncoder().encodeToString(bytes));
     }
 }
